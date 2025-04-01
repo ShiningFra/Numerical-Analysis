05af0247bbdf8ed2164ebe1317e1bcf3ee31b732
NGHOGUE TAPTUE Franck Roddier 21P279 ENSPY_3GI <165384649+RoddierFR@users.noreply.github.com>
2025-04-01 23:59:15 +0200

    Fra touched it again ...

--- src/main/java/com/fra/nan/FiniteVolumeDeduction.java
+++ src/main/java/com/fra/nan/FiniteVolumeDeduction.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.fra.nan;
 
 /**
  *
  * @author Roddier
  */
-public class NAn {
-
-    public static void main(String[] args) {
-        
-    }
+public class FiniteVolumeDeduction {
+    
 }
